fbac42142afe359c8fe98a111706c30214737086
Jamie Hardt <jamiehardt@me.com>
2022-06-14 13:11:58 -0700

    Display implementation

--- fhui/main_display.py
+++ fhui/main_display.py
@@ -1,2 +1,34 @@
+from typing import List
+
 class MainDisplay:
-    pass
+    display_cells: List[int]
+
+    def __init__(self):
+        display_zones = list()
+        for i in range(0,80):
+            display_cells[i] = 0x20
+
+    def line1(self) -> str:
+        retval = " " * 40
+        for i in range(0,40):
+            retval[i] = display_cells.get(i, " ")
+
+        return retval
+
+    def line2(self) -> str:
+        retval = " " * 40
+        for i in range(40,80):
+            retval[i] = display_cells.get(i, " ")
+
+        return retval
+
+    def update(self, zone: int, chars: List[int]):
+        if zone not in range(0,8) or len(chars) < 10:
+            #fixme this is an error
+            return
+        
+        for i in range(0,10):
+            display_cells[zone*10 + i] = chars[i]
+
+
+
--- fhui/time_display.py
+++ fhui/time_display.py
@@ -1,2 +1,45 @@
+from enum import IntFlag
+from typing import List
+
+
+class TimeDisplayUpdate:
+    digits: List[int]
+    decimals: List[bool]
+
+    @classmethod
+    def encode(cls, update: 'TimeDisplayUpdate') -> List[int]:
+        retval = list()
+        for i in range(0, len(update.digits)):
+            this_val = update.digits[i]
+            if update.decimal[i]:
+                this_val += 0x10
+            retval.append(this_val)
+
+        return retval
+            
+
+    @classmethod
+    def decode(cls, data: List[int]) -> 'TimeDisplayUpdate':
+        retval = TimeDisplayUpdate()
+
+        for i in data:
+            retval.digits.append(i & 0x0f)
+            retval.decimals.append(i & 0x10 > 0)
+
+        return retval
+
 class TimeDisplay:
-    pass
+    digits : List[int]
+    decimal : List[bool]
+    
+    @classmethod
+    def decode_digit(cls, val: int) -> str:
+        retval = ("%X" % (val))[0:1] 
+        return retval
+
+    def __init__(self):
+        self.digits = [0x0] * 8
+        self.decimal = [False] * 8
+
+    def update(self, update: TimeDisplayUpdate):
+        pass
