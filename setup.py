a69837a43774398834e3cbba3208d4a39b5240dc
Jamie Hardt <jamiehardt@me.com>
2022-06-13 21:20:18 -0700

    Setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+    name='fhui',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Mackie HUI control surface emuolation',
+    author='Jamie Hardt',
+    license='MIT',
+    install_requires='pygame',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
